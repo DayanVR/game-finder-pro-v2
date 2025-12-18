217e20f299f56e5a09d09a8d7430d73ee817e8f3
DayanVR <dayanvillavicencio13@gmail.com>
2025-12-18 11:59:03 -0500

    feat: Create Home page to fetch and display game details using `GameCard` component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+import { useFetchGamesData } from '@/features/games/hooks/fetchGamesData';
+import GameCard from '@/features/games/components/GameCard';
+
 export default function Home() {
+  const { gameDetails } = useFetchGamesData('halo-infinite');
+  console.log(gameDetails);
   return (
     <>
       <h1>Home</h1>
+      <div>
+        {gameDetails && (
+            <GameCard game={gameDetails} />
+        )}
+      </div>
     </>
   );
 }
