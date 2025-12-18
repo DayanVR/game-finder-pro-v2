2acf486e3773b8ce9abeb63c6d3a14901229491f
DayanVR <dayanvillavicencio13@gmail.com>
2025-12-18 11:58:32 -0500

    feat: Add game card component, game detail page, gamer library, and back navigation.

--- src/app/gamer-library/page.tsx
+++ src/app/gamer-library/page.tsx
@@ -1,19 +1,11 @@
-import { ArrowLeft } from '@/features/shared/gamerLibrary/leftArrow';
 import { TrashCan } from '@/features/shared/gamerLibrary/trashCan';
+import { NavigateBack } from '@/features/games/components/navigateBack';
 
 export default function GamerLibraryPage() {
   return (
     <>
       <section className="flex h-full w-full items-center justify-between">
-        <button
-          type="button"
-          className="flex w-fit cursor-pointer items-center space-x-3 rounded-full border border-white/10 px-6 py-3 backdrop-blur-md transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(139,92,246,0.5)]"
-        >
-          <ArrowLeft className="size-6 fill-(--color-accent-primary) transition-transform group-hover:scale-105" />
-          <span className="text-lg font-semibold text-white transition-colors xl:text-xl">
-            Back
-          </span>
-        </button>
+        <NavigateBack />
         <button
           type="button"
           //   onClick={handleClear}
@@ -26,7 +18,7 @@ export default function GamerLibraryPage() {
         </button>
       </section>
 
-      <section>
+      <section className="mt-6 flex h-full w-full items-center justify-center">
         <div>
           <h1 className="text-4xl font-bold">No games added to library</h1>
         </div>
---
+++ src/features/games/components/navigateBack.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ArrowLeft } from '@/features/shared/gamerLibrary/leftArrow';
+
+export const NavigateBack = () => {
+  const router = useRouter();
+  return (
+    <button
+      type="button"
+      onClick={router.back}
+      className="flex w-fit cursor-pointer items-center space-x-3 rounded-full border border-white/10 px-6 py-3 backdrop-blur-md transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(139,92,246,0.5)]"
+    >
+      <ArrowLeft className="size-6 fill-(--color-accent-primary) transition-transform group-hover:scale-105" />
+      <span className="text-lg font-semibold text-white transition-colors xl:text-xl">Back</span>
+    </button>
+  );
+};
