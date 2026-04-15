087800c748db52c4479956d3ef4b9a5c60e1abe9
DayanVR <dayanvillavicencio13@gmail.com>
2026-04-15 18:57:23 -0400

    feat: Update configuration files and class list for improved styling and functionality

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+import type { NextConfig } from 'next';
+import withFlowbiteReact from 'flowbite-react/plugin/nextjs';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://images.igdb.com/**')],
+  },
+  reactStrictMode: true,
 };
 
 export default withFlowbiteReact(nextConfig);
