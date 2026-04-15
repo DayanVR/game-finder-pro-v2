52c0ddb2d525d5b8e806a24fe4ee99fd89e8a30d
DayanVR <dayanvillavicencio13@gmail.com>
2026-04-15 19:00:01 -0400

    feat: Add ImageGallery component for displaying game screenshots in a carousel

---
+++ src/features/games/components/games/ImageGallery.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Carousel } from 'flowbite-react';
+import { normalizeImage } from '@/features/libs/normalizeImage';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function ImageGallery({ images }: { images: string[] }) {
+  const [imageIndex, setImageIndex] = useState(1);
+
+  const screenshotsList = images?.map((image) =>
+    normalizeImage(image.replace('t_thumb', 't_1080p'))
+  );
+  return (
+    <div className="space-y-4 py-4 lg:space-y-8 lg:py-8">
+      <h2 className="bg-linear-to-r from-(--color-accent-primary) to-(--color-accent-secondary) bg-clip-text text-center text-2xl font-bold text-transparent md:text-4xl xl:text-6xl">
+        CAPTURES
+      </h2>
+      <Carousel
+        onSlideChange={(index) => setImageIndex(index + 1)}
+        className="mx-auto h-56 w-11/12 sm:h-64 sm:w-8/12 md:w-8/12 lg:h-80 lg:w-8/12 xl:w-6/12 2xl:h-96 2xl:w-[600px]"
+        slide={true}
+        slideInterval={3000}
+        indicators={false}
+        pauseOnHover
+      >
+        {screenshotsList?.map((image, index) => (
+          <div key={index} className="h-full px-1 md:px-2">
+            <Image
+              src={image ?? '/img-not-found.jpg'}
+              alt="Game Screenshot"
+              width={500}
+              height={500}
+              className="size-full overflow-hidden"
+            />
+          </div>
+        ))}
+      </Carousel>
+      <div className="text-center">
+        <span className="text-2xl font-semibold lg:text-3xl">
+          {imageIndex} of {screenshotsList?.length}
+        </span>
+      </div>
+    </div>
+  );
+}
