14c00faa600e0a2151594951c27967b09b5d8bb5
DayanVR <dayanvillavicencio13@gmail.com>
2026-04-17 18:05:07 -0400

    fix: Update layout structure for improved styling and component arrangement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body suppressHydrationWarning className="ml-72 flex min-h-screen max-w-[1536px] flex-col bg-black pt-6">
-        <Header />
-        <SidePanel />
-        <main className="flex-1 px-12 py-6">{children}</main>
-        <footer className="bg-black p-6">footer</footer>
+    <html lang="en" className="flex justify-center bg-(--color-bg-secondary)">
+      <body suppressHydrationWarning className="flex min-h-screen w-full max-w-[1536px]">
+        <div className="sticky top-0 h-screen shrink-0">
+          <SidePanel />
+        </div>
+        <div className="w-full pt-7">
+          <Header />
+          <main className="px-12 py-6">{children}</main>
+          <footer className="bg-black p-6">footer</footer>
+        </div>
       </body>
     </html>
   );
