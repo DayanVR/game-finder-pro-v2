ea4aec0296830a1e0e69a0d6fb16305481e917fa
DayanVR <dayanvillavicencio13@gmail.com>
2026-04-19 21:18:26 -0400

    feat: Add IntroCall and InfoCall components to enhance layout with introductory and informational content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/shared/header/Header';
 import SidePanel from '@/shared/SidePanel';
+import IntroCall from '@/features/shared/IntroCall';
+import InfoCall from '@/features/shared/InfoCards';
 
 export const metadata: Metadata = {
   title: 'Game Finder Pro',
@@ -20,7 +22,9 @@ export default function RootLayout({
           <SidePanel />
         </div>
         <div className="w-full pt-7">
+          <IntroCall />
           <Header />
+          <InfoCall />
           <main className="px-12 py-6">{children}</main>
           <footer className="bg-black p-6">footer</footer>
         </div>
