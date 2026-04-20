db7c0a294384713ce640d332dc166655436623e2
DayanVR <dayanvillavicencio13@gmail.com>
2026-04-20 17:59:07 -0400

    feat: Update GameInfo layout and add game details buttons; enhance NavigateBack button shadow color

--- src/features/games/components/games/GameInfo.tsx
+++ src/features/games/components/games/GameInfo.tsx
@@ -3,6 +3,7 @@ import { IGDBGame } from '@/features/libs/types';
 import { useState } from 'react';
 import ImageGallery from './ImageGallery';
 import { formattedDate } from '@/features/libs/functions';
+import { detailsIcons } from '@/features/shared/gameDetailsIcons';
 
 export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
   const [gameInfo, setGameInfo] = useState<string | undefined>('summary');
@@ -19,6 +20,8 @@ export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
     screenshots,
     themes,
   } = gameDetails;
+  const { Star, Calendar, Controller, Desktop, Developer, Gallery, People, Suitcase } =
+    detailsIcons;
 
   const developerCompany = involved_companies?.find((company) => company.developer);
   const publisherCompany = involved_companies?.find((company) => company.publisher);
@@ -27,8 +30,78 @@ export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
   const videoUrl = `https://www.youtube.com/embed/${videos?.[0]?.video_id}`;
 
   return (
-    <>
-      <div className="grid grid-cols-2 gap-x-8">
+    <div className="grid grid-cols-3 grid-rows-4 gap-8">
+      <div className="col-span-2 mx-auto max-h-96 min-h-64 w-full rounded-2xl border border-white/20 py-5 xl:py-8">
+        <div className="flex justify-around pb-6 sm:justify-center md:space-x-8 lg:pt-4 xl:pt-6">
+          {summary && (
+            <>
+              <button
+                name="Summary"
+                className={`cursor-pointer ${gameInfo === 'summary' ? 'bg-(--color-accent-primary) text-white shadow-[0_0_10px_#e7000b]' : 'bg-dark-surface/50 hover:bg-dark-surface hover:border-(--color-accent-primary) hover:bg-(--color-accent-primary)/40'} rounded-full border border-white/10 px-6 py-3 transition-all`}
+                onClick={() => setGameInfo('summary')}
+              >
+                Summary
+              </button>
+            </>
+          )}
+          {gameDetails?.summary && gameDetails?.storyline && (
+            <div className="border-x border-(--color-text-secondary)/60"></div>
+          )}
+          {storyline && (
+            <>
+              <button
+                name="Storyline"
+                className={`cursor-pointer ${gameInfo === 'storyline' ? 'bg-(--color-accent-primary) text-white shadow-[0_0_10px_#e7000b]' : 'bg-dark-surface/50 hover:bg-dark-surface hover:border-(--color-accent-primary) hover:bg-(--color-accent-primary)/40'} rounded-full border border-white/10 px-6 py-3 transition-all`}
+                onClick={() => setGameInfo('storyline')}
+              >
+                Storyline
+              </button>
+            </>
+          )}
+        </div>
+        {gameInfo === 'summary' && <p className="line-clamp-6 xl:mx-7 2xl:text-xl">{summary}</p>}
+        {gameInfo === 'storyline' && (
+          <p className="line-clamp-6 xl:mx-7 2xl:text-xl">{storyline}</p>
+        )}
+      </div>
+      <div className="col-span-1 flex flex-col justify-center gap-4 rounded-2xl border border-white/20 px-6">
+        <div className="flex items-center gap-4">
+          <div className="rounded-lg bg-(--color-accent-primary)/20 p-3">
+            <Developer className="size-4 text-(--color-accent-secondary)" />
+          </div>
+          <div>
+            <h3 className="text-white/60">DEVELOPER</h3>
+            <span className="font-bold">{developerCompany?.company.name}</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="rounded-lg bg-(--chart-5)/10 p-3">
+            <Suitcase className="size-4 text-(--chart-5)" />
+          </div>
+          <div>
+            <h3 className="text-white/60">PUBLISHER</h3>
+            <span className="font-bold">{publisherCompany?.company.name}</span>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-1 row-span-1 rounded-2xl border border-white/20">sadasddsa</div>
+      <div className="col-span-2 row-span-3 row-start-2 h-fit rounded-2xl border border-white/20">
+        {videos && (
+          <div className="space-y-4 py-4 lg:space-y-6 lg:py-6">
+            <h2 className="mx-6 text-xl font-semibold text-(--color-accent-primary) md:text-2xl xl:text-4xl">
+              GAMEPLAY <span className="text-white">TRAILER</span>
+            </h2>
+            <iframe
+              className="mx- aspect-video h-80 w-full min-w-full"
+              src={videoUrl}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            />
+          </div>
+        )}
+      </div>
+      {/* <div className="grid grid-cols-2 gap-x-8">
         <dl className="space-y-2 text-pretty [&_dd]:pt-2 [&_dt]:pt-2 [&_dt]:font-semibold">
           {themes?.length !== undefined && themes?.length > 0 && (
             <>
@@ -76,7 +149,7 @@ export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
           {genres?.length !== undefined && genres?.length > 0 && (
             <>
               <dt>Genres:</dt>
-              <dd>{genres?.map((genre) => genre.name).join(', ')}</dd>
+              <dd></dd>
             </>
           )}
           {platforms?.length !== undefined && platforms?.length > 0 && (
@@ -93,7 +166,7 @@ export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
             <>
               <button
                 name="Summary"
-                className={`cursor-pointer ${gameInfo === 'summary' ? 'bg-(--color-accent-primary) text-white shadow-[0_0_10px_rgba(139,92,246,0.5)]' : 'bg-dark-surface/50 hover:bg-dark-surface hover:border-(--color-accent-primary) hover:bg-(--color-accent-primary)/40'} rounded-full border border-white/10 px-6 py-3 transition-all`}
+                className={`cursor-pointer ${gameInfo === 'summary' ? 'bg-(--color-accent-primary) text-white shadow-[0_0_10px_#e7000b]' : 'bg-dark-surface/50 hover:bg-dark-surface hover:border-(--color-accent-primary) hover:bg-(--color-accent-primary)/40'} rounded-full border border-white/10 px-6 py-3 transition-all`}
                 onClick={() => setGameInfo('summary')}
               >
                 Summary
@@ -107,7 +180,7 @@ export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
             <>
               <button
                 name="Storyline"
-                className={`cursor-pointer ${gameInfo === 'storyline' ? 'bg-(--color-accent-primary) text-white shadow-[0_0_10px_rgba(139,92,246,0.5)]' : 'bg-dark-surface/50 hover:bg-dark-surface hover:border-(--color-accent-primary) hover:bg-(--color-accent-primary)/40'} rounded-full border border-white/10 px-6 py-3 transition-all`}
+                className={`cursor-pointer ${gameInfo === 'storyline' ? 'bg-(--color-accent-primary) text-white shadow-[0_0_10px_#e7000b]' : 'bg-dark-surface/50 hover:bg-dark-surface hover:border-(--color-accent-primary) hover:bg-(--color-accent-primary)/40'} rounded-full border border-white/10 px-6 py-3 transition-all`}
                 onClick={() => setGameInfo('storyline')}
               >
                 Storyline
@@ -138,7 +211,7 @@ export default function GameInfo({ gameDetails }: { gameDetails: IGDBGame }) {
           </div>
         )}
         {screenshots && <ImageGallery images={screenshots.map((screenshot) => screenshot.url)} />}
-      </div>
-    </>
+      </div> */}
+    </div>
   );
 }
--- src/features/games/components/navigateBack.tsx
+++ src/features/games/components/navigateBack.tsx
@@ -9,7 +9,7 @@ export const NavigateBack = () => {
     <button
       type="button"
       onClick={router.back}
-      className="flex w-fit cursor-pointer items-center space-x-3 rounded-full border border-white/10 px-6 py-3 backdrop-blur-md transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_rgba(139,92,246,0.5)]"
+      className="flex w-fit cursor-pointer items-center space-x-3 rounded-full border border-white/10 px-6 py-3 backdrop-blur-md transition-all duration-300 hover:scale-105 hover:shadow-[0_0_15px_#e7000b]"
     >
       <ArrowLeft className="size-6 fill-(--color-accent-primary) transition-transform group-hover:scale-105" />
       <span className="text-lg font-semibold text-white transition-colors xl:text-xl">Back</span>
