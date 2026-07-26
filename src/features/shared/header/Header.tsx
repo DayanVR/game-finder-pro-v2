f38fe96b99c2eab90527913f5bf8b60e177af6b4
DayanVR <dayanvillavicencio13@gmail.com>
2026-07-26 16:09:32 -0400

    Change from Sign In to Login

--- src/features/shared/header/Header.tsx
+++ src/features/shared/header/Header.tsx
@@ -4,31 +4,31 @@ import SearchForm from './SearchForm';
 
 export default function Header() {
   return (
-    <header className="mx-10 lg:mx-12 flex items-center justify-between gap-y-5 max-lg:mt-12 max-lg:flex-col lg:mt-8 xl:mt-12">
+    <header className="mx-10 flex items-center justify-between gap-y-5 max-lg:mt-12 max-lg:flex-col lg:mx-12 lg:mt-8 xl:mt-12">
       <SearchForm />
       <div className="flex items-center gap-4 lg:gap-10">
         <Show when="signed-in">
           <GamerLibraryLink />
-          <UserButton  />
+          <UserButton />
         </Show>
         <Show when="signed-out">
           <div className="ml-2 hidden items-center gap-3 pl-4 sm:flex">
             <SignInButton>
               <button
                 type="button"
-                className="h-10 cursor-pointer rounded-full bg-gray-300/10 px-4 text-sm font-medium text-white transition-all hover:bg-gray-300/30 sm:h-12 sm:px-5 sm:text-base"
+                className="h-10 cursor-pointer rounded-full bg-(--color-accent-primary) px-4 text-sm font-medium text-white transition-all hover:bg-(--color-accent-primary)/80 hover:shadow-[0_0_25px_#e7000b] sm:h-12 sm:px-5 sm:text-base"
               >
-                Sign In
+                Login
               </button>
             </SignInButton>
-            <SignUpButton>
+            {/* <SignUpButton>
               <button
                 type="button"
                 className="h-10 cursor-pointer rounded-full bg-(--color-accent-primary) px-4 text-sm font-medium text-white transition-all hover:bg-(--color-accent-primary)/80 hover:shadow-[0_0_25px_#e7000b] sm:h-12 sm:px-5 sm:text-base"
               >
                 Sign Up
               </button>
-            </SignUpButton>
+            </SignUpButton> */}
           </div>
         </Show>
       </div>
